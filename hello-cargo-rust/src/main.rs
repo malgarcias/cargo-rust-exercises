6caa0c0089bbe3a846e951cfe269b7043ab54476
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2020-05-14 14:17:41 -0400

    Revert "changed message"
    This reverts commit 25ec84ceaee8a314d5ce660fca3234acdaa2a03f.

--- hello-cargo-rust/src/main.rs
+++ hello-cargo-rust/src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, Rusty world!");
+    println!("Hello, world!");
 }
--- hello-cargo-rust/target/debug/hello-cargo-rust.d
+++
@@ -1,1 +0,0 @@
-/Users/marcelogarcia/repos/cargo-rust-exercises/hello-cargo-rust/target/debug/hello-cargo-rust: /Users/marcelogarcia/repos/cargo-rust-exercises/hello-cargo-rust/src/main.rs

25ec84ceaee8a314d5ce660fca3234acdaa2a03f
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2020-05-14 14:16:17 -0400

    changed message

---
+++ hello-cargo-rust/target/debug/deps/hello_cargo_rust-13ff353e4c600135.d
@@ -0,0 +1,5 @@
+/Users/marcelogarcia/repos/cargo-rust-exercises/hello-cargo-rust/target/debug/deps/hello_cargo_rust-13ff353e4c600135: src/main.rs
+
+/Users/marcelogarcia/repos/cargo-rust-exercises/hello-cargo-rust/target/debug/deps/hello_cargo_rust-13ff353e4c600135.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo-rust/target/debug/hello-cargo-rust.d
@@ -0,0 +1,1 @@
+/Users/marcelogarcia/repos/cargo-rust-exercises/hello-cargo-rust/target/debug/hello-cargo-rust: /Users/marcelogarcia/repos/cargo-rust-exercises/hello-cargo-rust/src/main.rs
